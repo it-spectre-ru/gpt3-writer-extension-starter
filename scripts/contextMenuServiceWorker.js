7fa855d45fd4546cc0a4503a65d84c792219c51e
it-spectre-ru <it-spectre-ru@gmail.com>
2023-02-13 18:12:12 +0500

    generate

--- scripts/contextMenuServiceWorker.js
+++ scripts/contextMenuServiceWorker.js
@@ -1,5 +1,30 @@
-// Setup our generate function
-const generate = async (prompt) => {};
+// Function to get + decode API key
+const getKey = () => {};
+
+const generate = async (prompt) => {
+  // Get your API key from storage
+  const key = await getKey();
+  const url = 'https://api.openai.com/v1/completions';
+
+  // Call completions endpoint
+  const completionResponse = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${key}`,
+    },
+    body: JSON.stringify({
+      model: 'text-davinci-003',
+      prompt: prompt,
+      max_tokens: 1250,
+      temperature: 0.7,
+    }),
+  });
+
+  // Select the top choice and send back
+  const completion = await completionResponse.json();
+  return completion.choices.pop();
+};
 
 const generateCompletionAction = async (info) => {
   try {
