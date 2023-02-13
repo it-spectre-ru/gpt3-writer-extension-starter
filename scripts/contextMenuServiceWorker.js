bdc6dcceef81504644f92189aff059b3fb7114c7
it-spectre-ru <it-spectre-ru@gmail.com>
2023-02-13 17:49:20 +0500

    add listener

---
+++ scripts/contextMenuServiceWorker.js
@@ -0,0 +1,11 @@
+// Add this in scripts/contextMenuServiceWorker.js
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'context-run',
+    title: 'Generate blog post',
+    contexts: ['selection'],
+  });
+});
+
+// Add listener
+chrome.contextMenus.onClicked.addListener(generateCompletionAction);
